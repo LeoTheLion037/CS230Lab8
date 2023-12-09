867e0c76e42b18900df153938ebe743a5de6701b
LeoTheLion037 <leonardopeele@gmail.com>
2023-12-09 14:36:45 -0500

    working todo list

--- amplifyapp3/src/App.js
+++ amplifyapp3/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useRef, useState } from "react";
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const inputRef = useRef(null);
+
+  const addItem = (e) => {
+    e.preventDefault();
+    const newItem = inputRef.current.value;
+    if (newItem !== "") {
+      setTasks([...tasks, newItem]);
+    }
+    inputRef.current.value = "";
+  };
+
+  const removeItem = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="custom-container">
+      <header className="custom-header">
+        <h1>My Custom ToDo List</h1>
       </header>
+      <main className="custom-main">
+        <div className="custom-input">
+          <input type="text" placeholder="Enter an item" ref={inputRef} />
+          <button className="custom-add-btn" onClick={addItem}>
+            Add
+          </button>
+        </div>
+        <ul className="custom-list">
+          {tasks.map((task, index) => (
+            <li className="custom-list-item" key={index}>
+              {task}
+              <button className="custom-delete-btn" onClick={() => removeItem(index)}>
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      </main>
+      <footer className="custom-footer">
+        <p>© 2023 My ToDo List App</p>
+      </footer>
     </div>
   );
 }
